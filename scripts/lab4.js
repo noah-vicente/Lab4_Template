77d7f490b8fa2f6942f634495cf8fcb1ce5b5829
nvicente <nvicente@ucsd.edu>
2026-04-24 15:43:54 -0700

    corrected bug fix in lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -28,13 +28,15 @@ function discountPrices(prices, discount) {
     const discounted = [];
     const length = prices.length;
     let discountedPrice = 0;
+
+    if(length === 0) return false;
     for(let i = 0; i < length; i++) {
         discountedPrice = prices[i] * (1 - discount);
         const finalPrice = Math.round(discountedPrice * 100) / 100;
         discounted.push(finalPrice);
     }
     
-    if(discounted.length == 0) return false;
+    
     return discounted;
 }
 
